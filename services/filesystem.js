283616e0c3d46b0221fc80ee79925d2b6e1fb6e6
is-pooya <ispooya@gmail.com>
2022-04-25 18:47:52 +0430

    refactor(filesystems): add getFile and getDirectory services to filesystem

---
+++ services/filesystem.js
@@ -0,0 +1,8 @@
+const getFile = (address) => {
+  return ["hi", "im pooya", "new line"];
+};
+const getDirectory = (adsress) => {
+  return ["index.js"];
+};
+
+module.exports = { getFile, getDirectory };
