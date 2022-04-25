56e537fbf1691bff361abcc91f60dd9124cc8562
is-pooya <ispooya@gmail.com>
2022-04-25 18:48:23 +0430

    feat(commit api): add commit api

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 const express = require("express");
+const { getFile, getDirectory } = require("./services/filesystem");
+const { getFileFromMergedRepo, addNewCommita } = require("./services/database");
 const app = express();
 const port = 3000;
 
-app.get("/commit", (req, res) => {});
+app.get("/commit", (req, res) => {
+  var dir = getDirectory("/");
+
+  var changes = [];
+
+  dir.forEach((address) => {
+    let oldFile = getFileFromMergedRepo(address);
+
+    let changedFile = getFile(address);
+
+    // each
+    changedFile.forEach((line, index) => {
+      //changes
+      if (line !== oldFile[index]) {
+        console.log(" ");
+
+        changes.push({
+          lineNumber: index + 1,
+          From: oldFile[index],
+          changeTo: line,
+          type: "newline",
+        });
+      }
+    });
+    console.log(changes);
+  });
+});
 
 app.listen(port, () => {
   console.log(`VCS app listening on port ${port}`);
