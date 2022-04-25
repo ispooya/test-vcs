055aabd6b0e601c82595b11639f063636452aaa5
is-pooya <ispooya@gmail.com>
2022-04-25 16:28:26 +0430

    init base structure

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/commit", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
