0a0667a62fe549144d94129926c575f2822757ea
is-pooya <ispooya@gmail.com>
2022-04-25 18:46:11 +0430

    feat(database service): create services/database.js

---
+++ services/database.js
@@ -0,0 +1,6 @@
+const addNewCommit = (repoId, message, commitType) => {};
+const getFileFromMergedRepo = (repoId, fileAddress) => {
+  return ["hello", "im hosein"];
+};
+
+module.exports = { addNewCommit, getFileFromMergedRepo };
